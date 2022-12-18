fa35ce45536d83d45ac31097f528f893c529d04f
ednukros <isanmenarguez@gmail.com>
2022-12-18 14:12:44 +0100

    iteration#4 mejorada

--- iteration4.js
+++ iteration4.js
@@ -1,15 +1,15 @@
 // Iteración #4: Calcular el promedio
 
 const numbers = [12, 21, 38, 5, 45, 37, 6];
-function average(param) {
+function average(array) {
   
     let total = 0;
-for (const number of numbers) {
+for (const number of array) {
     
     total += number;
 }
 
-console.log(total/numbers.length);
+console.log(total/array.length);
 }
 
-average();
+average(numbers);
