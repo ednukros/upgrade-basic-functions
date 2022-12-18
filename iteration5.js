1c9f7baf8d8787c4dfee079f9d3a5ad62ed04bde
ednukros <isanmenarguez@gmail.com>
2022-12-18 12:46:47 +0100

    iteration#5

---
+++ iteration5.js
@@ -0,0 +1,25 @@
+//Iteracion 5: Calcular promedio de Strings
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+function averageWord(param) {
+ 
+    let total =0;
+
+    for (const element of mixedElements) {
+        
+        if(typeof element === 'number'){
+
+            total += element;
+        }else{
+
+            total += element.length;
+        }
+
+    }
+
+    console.log(total);
+
+}
+
+averageWord(mixedElements);
+
