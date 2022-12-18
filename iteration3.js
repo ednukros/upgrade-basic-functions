0e6d33b4424d27e47fd3469fd81c5ff7c284ad7d
ednukros <isanmenarguez@gmail.com>
2022-12-18 12:02:31 +0100

    iteration#3

---
+++ iteration3.js
@@ -0,0 +1,20 @@
+//Iteracion 3: Calcular la suma
+
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(param) {
+  
+    let suma = 0;
+   for (const number of numbers) {
+
+    suma += number;
+
+    
+   }
+
+   console.log(suma);
+}
+
+sumAll();
+
+
